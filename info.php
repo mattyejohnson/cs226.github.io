86fbf483abb123593bb7683bc6886a363cf80ac6
mattyejohnson <46620501+mattyejohnson@users.noreply.github.com>
2019-11-20 23:16:43 -0800

    Update info.php

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysql_connect(“test”, “root”, “sid16378”); if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db(“database_name”, $connect);
+$connect = mysql_connect(“test”, “root”); if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db(“database_name”, $connect);
 $user_info = “INSERT INTO table_name (username, email) VALUES ('$_POST[username]', '$_POST[email]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
 
 echo “Your information was added to the database.”;
